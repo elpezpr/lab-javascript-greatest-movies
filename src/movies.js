f01dea6454ec4e9067e30e0100d2b7dfed8ebf44
elpezpr <gabrielpena@hotmail.com>
2024-07-19 11:36:41 -0400

    added orderByYear and orderAlphabetically

--- src/movies.js
+++ src/movies.js
@@ -34,10 +34,26 @@ function dramaMoviesScore(moviesArray) {
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+    const sortedMovies = [...moviesArray];
+
+    sortedMovies.sort((a, b) => {
+        if (a.year === b.year) {
+            return a.title.localeCompare(b.title);
+        }
+        return a.year - b.year;
+    });
+
+    return sortedMovies;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+    return [...moviesArray]
+        .sort((a, b) => a.title.localeCompare(b.title))
+        .slice(0, 20)
+        .map(movie => movie.title);    
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
